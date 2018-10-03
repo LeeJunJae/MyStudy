d5b179c533bf5dddb1a0bcf03b45aae186ed5294
이준재 <wnswo158@naver.com>
2018-10-03 16:24:50 +0900

    44

--- Level_45_LAST/03.cpp
+++ Level_45_LAST/03.cpp
@@ -1,13 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <string.h>
 
+char strA[10] = "ABCDE";
+char strB[10] = "BBCDE";
 
+char aSentence[4][3];
+char bSentence[4][3];
 
+void DivideSentence(char* data, char* dSentence)
+{
+	strcpy(dSentence, data);	
+}
 
 
 int main()
 {
-
-
+	for (int i = 0; i < strlen(strA) - 1; i++)
+	{		
+		DivideSentence(aSentence[i], strA);
+	}
+	
 
 	return 0;
 }
